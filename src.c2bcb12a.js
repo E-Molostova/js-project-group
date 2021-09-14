b85384757f79f78718e3f71707de77eec29fc081
E-Molostova <86022867+E-Molostova@users.noreply.github.com>
2021-09-14 07:19:09 +0000

    Deploying to gh-pages from @ E-Molostova/js-project-group@ca2f706df658c487e893eebd27e5fe4aeddc60f2 🚀

--- src.c2bcb12a.js
+++ src.c2bcb12a.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./api-service":"hC31"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/api-service"),require("./js/gallery");
 },{"./sass/main.scss":"clu1","./js/api-service":"hC31","./js/gallery":"chxP"}]},{},["Focm"], null)
-//# sourceMappingURL=/js-project-group/src.fe4652bd.js.map
+//# sourceMappingURL=/js-project-group/src.c2bcb12a.js.map
