b093d292d3f55c93f7303c8a7725ce9aea67b917
Vladyslav Turchenko <86655268+agatohn@users.noreply.github.com>
2021-09-15 11:40:17 +0300

    Commit

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,14 +1,14 @@
 import ApiService from './api-service';
 import makeMoviesMarkup from '../templates/movieList.hbs';
+import makeMoviesById from '../templates/modalMovie.hbs';
 import refs from './refs';
-// import Handlebars from 'handlebars';
 
 const api = new ApiService();
 let genres = [];
 const fetch = () => {
   api.fetchGenres().then(data => {
     console.log(data);
-    getTrendingMovies();
+    // getTrendingMovies();
     return saveGenresList(data);
   });
 };
@@ -18,43 +18,83 @@ const saveGenresList = data => {
 console.log(genres);
 fetch();
 
-const getTrendingMovies = () => {
+// const getTrendingMovies = () => {
+//   api
+//     .fetchTrending()
+//     .then(data => {
+//       // console.log(data);
+//       renderMovies(data);
+//     })
+//     .catch(err => {
+//       console.log(err.message);
+//     });
+// };
+
+// const renderMovies = ({ results }) => {
+//   const normilizedResults = results.map(movie => {
+//     let releaseYear = 'Unknown';
+//     if (Date.parse(movie.release_date)) {
+//       releaseYear = new Date(movie.release_date).getFullYear();
+//     }
+//     const iconFullPath = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+//     const poster = movie.poster_path ? iconFullPath : emptyImg;
+//     // console.log(genres);
+//     // console.log(movie.genre_ids);
+//     const genresNames = movie.genre_ids.map(id => genres.find(x => x.id === id).name).join(', ');
+//     return {
+//       ...movie,
+//       release_date: releaseYear,
+//       poster_path: poster,
+//       genres: genresNames,
+//     };
+//   });
+//   console.log(normilizedResults);
+//   // console.log(makeMoviesMarkup);
+//   const markup = makeMoviesMarkup(normilizedResults);
+//   refs.galleryList.insertAdjacentHTML('beforeend', markup);
+//   // refs.galleryList.innerHTML = markup;
+// };
+
+const getMoviesById = () => {
+  api.movId = 385128;
   api
-    .fetchTrending()
+    .fetchMovieById()
     .then(data => {
-      // console.log(data);
-      renderMovies(data);
+      console.log(data);
+      rendermodal(data);
     })
     .catch(err => {
       console.log(err.message);
     });
 };
 
-const renderMovies = ({ results }) => {
-  const normilizedResults = results.map(movie => {
-    let releaseYear = 'Unknown';
-    if (Date.parse(movie.release_date)) {
-      releaseYear = new Date(movie.release_date).getFullYear();
-    }
-    const iconFullPath = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
-    const poster = movie.poster_path ? iconFullPath : emptyImg;
-    // console.log(genres);
-    // console.log(movie.genre_ids);
-    const genresNames = movie.genre_ids.map(id => genres.find(x => x.id === id).name).join(', ');
-    return {
-      ...movie,
-      release_date: releaseYear,
-      poster_path: poster,
-      genres: genresNames,
-    };
-  });
-  console.log(normilizedResults);
-  // console.log(makeMoviesMarkup);
-  const markup = makeMoviesMarkup(normilizedResults);
+const rendermodal = results => {
+  const normilizedResult = preparingData(results);
+
+  console.log(normilizedResult);
+  const markup = makeMoviesById(normilizedResult);
   refs.galleryList.insertAdjacentHTML('beforeend', markup);
-  // refs.galleryList.innerHTML = markup;
 };
 
+function preparingData(result) {
+  console.log(result);
+  let releaseYear = 'Unknown';
+  if (Date.parse(result.release_date)) {
+    releaseYear = new Date(result.release_date).getFullYear();
+  }
+  const iconFullPath = `https://image.tmdb.org/t/p/w500${result.poster_path}`;
+  const poster = result.poster_path ? iconFullPath : emptyImg;
+  // console.log(genres);
+  // console.log(movie.genre_ids);
+  const genresNames = result.genres.map(genre => genre.name).join(', ');
+  return {
+    ...result,
+    release_date: releaseYear,
+    poster_path: poster,
+    genres: genresNames,
+  };
+}
+getMoviesById();
 // const fetchTr = () => {
 //   api.fetchTrending().then(data => {
 //     console.log(data);
